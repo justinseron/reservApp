4806a10f0992598a7de9f672f64000b7223f0f0f
justinseron <ju.seron@duocuc.cl>
2024-09-03 12:45:57 -0400

    modificamos formulario, hicimos login, tabs

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +11,29 @@ export class LoginPage implements OnInit {
   //aquí podemos crear variables, constantes, listas, arreglos, json, etc:
   //NOMBRE_VAR: TIPO = VALOR;
   titulo: string = "PÁGINA DE LOGIN";
-  numero: number = 5;
+  /*numero: number = 5;
   decimal: number = 5.2;
   existe: boolean = true;
   fecha_hoy: Date = new Date();
   nombres: string[] = ["Pedro","Juan","Diego"];
-  persona: any = {"nombre":"Jason", "edad":5}
+  persona: any = {"nombre":"Jason", "edad":5}*/
 
   //NgModel:
   email: string = "";
+  password: string = "";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  //método asociado al boton para hacer un login:
+  login(){
+    if(this.email=="pavel@gmail.com" && this.password=="Jiulliet"){
+      this.router.navigate(['/home'])
+    }else{
+      alert("¡Correo o contraseña incorrectos!")
+    }
   }
 
 }
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -14,11 +14,32 @@ export class RegistroPage implements OnInit {
   //NOMBRE = Clase(VALOR);
   persona = new FormGroup({
     rut: new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
-    nombre: new FormControl('',[Validators.minLength(3),Validators.required,Validators.pattern("[a-z]{3,5}")]),
-    fecha_nacimiento: new FormControl(),
-    genero: new FormControl()
+    nombre: new FormControl('',[Validators.required,Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento: new FormControl('',[Validators.required]),
+    genero: new FormControl('',[Validators.required]),
+    tiene_equipo: new FormControl('no',[Validators.required]),
+    nombre_equipo: new FormControl('',[])
   });
 
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+
+  //Habrán validaciones que no existirán entre las librerias, por lo que tendremos que crear métodos propios para poder validar
+  //***HACER METODO PARA VALIDAR FECHA***
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -27,8 +48,18 @@ export class RegistroPage implements OnInit {
   //Aquí podemos crear métodos:
   registrar():void{
     console.log(this.persona.value);
-    alert("¡Registrado!");
     this.router.navigate(['/login']); 
+  }
+
+  /*habilitar_boton():boolean{
+    if(this.persona.valid){
+      return false;
+    }
+    return true;
+  }*/
+
+  setResult(ev:any) {
+    console.log(`Dismissed with role: ${ev.detail.role}`);
   }
 
 }
